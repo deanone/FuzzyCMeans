2362e0e7433a377e217cc896b7b1d36e8b8d5f84
Athanasios Salamanis <asal@iti.gr>
2021-07-13 12:50:29 +0300

    1. FuzzyCMeans class has been refactored.

--- FuzzyCMeans.cpp
+++ FuzzyCMeans.cpp
@@ -8,152 +8,167 @@
 #include <iostream>
 #include <ctime>
 
-FuzzyCMeans::FuzzyCMeans(std::string dataset_filename, std::string properties_fileName)
+FuzzyCMeans::FuzzyCMeans(std::string dataFilename, std::string paramsFilename)
 {
 	srand(static_cast<unsigned int>(time(NULL)));
-	PropertiesParser pp(properties_fileName);
+	PropertiesParser pp(paramsFilename);
 	dimension = pp.getPropertyAsIntOrDefaultTo("dimension", 4);
 	k = pp.getPropertyAsIntOrDefaultTo("k", 200);
-	max_num_of_iterations = pp.getPropertyAsIntOrDefaultTo("max_num_of_iterations", 1000);
+	maxNumOfIterations = pp.getPropertyAsIntOrDefaultTo("maxNumOfIterations", 1000);
 	m = pp.getPropertyAsIntOrDefaultTo("m", 2);
 	eps = pp.getPropertyAsDoubleOrDefaultTo("eps", 1e-6);
+	
 	std::string dataline;
 	std::ifstream in;
-	in.open(dataset_filename);
+	in.open(dataFilename);
 	if (in.is_open())
 	{
 		while (std::getline(in, dataline))
 		{
 			std::stringstream ss(dataline);
 			std::string item;
-			std::vector<std::string> items;
+			StringVector items;
 			while (getline(ss, item, ','))
+			{
 				items.push_back(item);
+			}
 			int ID = std::stoi(items[0]);
 			Point point(ID, k);
 			// TODO: instead of adding value in each iteration by calling addValue function,
 			// initialize values vector in the constructor and then change the values in each iteration
-			for (size_t i = 1; i < items.size(); ++i)
+			for (size_t i = 1; i < items.size(); ++i) 
+			{
 				point.addValue(std::stod(items[i]));
+			}
 			items.clear();
 			points.push_back(point);
 		}
 		in.close();
 	}
-	for (size_t cluster_id = 0; cluster_id < k; ++cluster_id)
+	for (size_t clusterID = 0; clusterID < k; ++clusterID)
 	{
-		std::vector<double> initial_centroid;
-		initial_centroid.assign(dimension, 0.0);
-		Cluster cl(initial_centroid, cluster_id);
+		DoubleVector initialCentroid;
+		initialCentroid.assign(dimension, 0.0);
+		Cluster cl(initialCentroid, clusterID);
 		clusters.push_back(cl);
 	}
 }
 
-FuzzyCMeans::FuzzyCMeans(std::string dataset_filename, int dimension_, int k_, int max_num_of_iterations_, int m_, double eps_) :
-	dimension(dimension_), k(k_), max_num_of_iterations(max_num_of_iterations_), m(m_), eps(eps_)
+FuzzyCMeans::FuzzyCMeans(std::string dataFilename, int dimension_, int k_, int maxNumOfIterations_, int m_, double eps_) :
+	dimension(dimension_), k(k_), maxNumOfIterations(maxNumOfIterations_), m(m_), eps(eps_)
 {
 	srand(static_cast<unsigned int>(time(NULL)));
+	
 	std::string dataline;
 	std::ifstream in;
-	in.open(dataset_filename);
+	in.open(dataFilename);
 	if (in.is_open())
 	{
 		while (std::getline(in, dataline))
 		{
 			std::stringstream ss(dataline);
 			std::string item;
-			std::vector<std::string> items;
-			while (getline(ss, item, ','))
+			StringVector items;
+			while (getline(ss, item, ',')) 
+			{
 				items.push_back(item);
+			}
 			int ID = std::stoi(items[0]);
 			Point point(ID, k);
-			for (size_t i = 1; i < items.size(); ++i)
+			for (size_t i = 1; i < items.size(); ++i) 
+			{
 				point.addValue(std::stod(items[i]));
+			}
 			items.clear();
 			points.push_back(point);
 		}
 		in.close();
 	}
-	for (size_t cluster_id = 0; cluster_id < k; ++cluster_id)
+	for (size_t clusterID = 0; clusterID < k; ++clusterID)
 	{
-		std::vector<double> initial_centroid;
-		initial_centroid.assign(dimension, 0.0);
-		Cluster cl(initial_centroid, cluster_id);
+		DoubleVector initialCentroid;
+		initialCentroid.assign(dimension, 0.0);
+		Cluster cl(initialCentroid, clusterID);
 		clusters.push_back(cl);
 	}
 }
 
 FuzzyCMeans::~FuzzyCMeans()
 {
-	if (!points.empty()) points.clear();
-	if (!clusters.empty()) clusters.clear();
+	if (!points.empty())
+	{
+		points.clear();
+	}
+	if (!clusters.empty()) 
+	{
+		clusters.clear();
+	}
 }
 
 void FuzzyCMeans::computeClusters()
 {
-	for (size_t cluster_id = 0; cluster_id < k; ++cluster_id)
+	for (size_t clusterID = 0; clusterID < k; ++clusterID)
 	{
-		std::vector<double> nominator;
+		DoubleVector nominator;
 		nominator.assign(dimension, 0.0);
 		double denominator = 0.0;
-		for (size_t point_id = 0; point_id < points.size(); ++point_id)
+		for (size_t pointID = 0; pointID < points.size(); ++pointID)
 		{
-			double w_old_value = points[point_id].getWOldValue(cluster_id);
-			w_old_value = std::pow(w_old_value, m);
-			denominator += w_old_value;
-			std::vector<double> temp = mfnc::multiplyVectorByConstant(*(points[point_id].getValues()), w_old_value);
+			double wOldValue = points[pointID].getWOldValue(clusterID);
+			wOldValue = std::pow(wOldValue, m);
+			denominator += wOldValue;
+			DoubleVector temp = mfnc::multiplyVectorByConstant(*(points[pointID].getValues()), wOldValue);
 			mfnc::addToVector(nominator, temp);
 			temp.clear();
 		}
-		std::vector<double> centroid = mfnc::multiplyVectorByConstant(nominator, 1.0 / denominator);
-		clusters[cluster_id].setCentroid(centroid);
+		DoubleVector centroid = mfnc::multiplyVectorByConstant(nominator, 1.0 / denominator);
+		clusters[clusterID].setCentroid(centroid);
 	}
 }
 
 void FuzzyCMeans::computeNewMembershipCoefficients()
 {
-	for (size_t point_id = 0; point_id < points.size(); ++point_id)
+	for (size_t pointID = 0; pointID < points.size(); ++pointID)
 	{
-		std::vector<double> distances;
-		for (size_t cluster_id = 0; cluster_id < clusters.size(); ++cluster_id)
+		DoubleVector distances;
+		for (size_t clusterID = 0; clusterID < clusters.size(); ++clusterID)
 		{
-			//dist = mfnc::ComputeEuclideanDistance(*(points[point_id].getValues()), *center);
-			double dist = mfnc::computeEuclideanDistance(*(points[point_id].getValues()), *(clusters[cluster_id].getCentroid()));
+			//dist = mfnc::ComputeEuclideanDistance(*(points[pointID].getValues()), *center);
+			double dist = mfnc::computeEuclideanDistance(*(points[pointID].getValues()), *(clusters[clusterID].getCentroid()));
 			distances.push_back(dist);
 		}
-		std::vector<double> w_new;
-		for (size_t cluster_id1 = 0; cluster_id1 < clusters.size(); ++cluster_id1)
+		DoubleVector wNew;
+		for (size_t firstOtherClusterID = 0; firstOtherClusterID < clusters.size(); ++firstOtherClusterID)
 		{
-			double currentClusterDistance = distances[cluster_id1];
+			double currentClusterDistance = distances[firstOtherClusterID];
 			double distancesSum = 0.0;
-			for (size_t cluster_id2 = 0; cluster_id2 < clusters.size(); ++cluster_id2)
+			for (size_t secondOtherClusterID = 0; secondOtherClusterID < clusters.size(); ++secondOtherClusterID)
 			{
-				double otherClusterDistance = distances[cluster_id2];
+				double otherClusterDistance = distances[secondOtherClusterID];
 				distancesSum += std::pow(currentClusterDistance / otherClusterDistance, 2 / (m - 1));
 			}
-			w_new.push_back(1.0 / distancesSum);
+			wNew.push_back(1.0 / distancesSum);
 		}
 		distances.clear();
-		points[point_id].updateWNew(w_new);
-		w_new.clear();
+		points[pointID].updateWNew(wNew);
+		wNew.clear();
 	}
 }
 
 void FuzzyCMeans::updateMembershipCoefficients()
 {
-	for (size_t point_id = 0; point_id < points.size(); ++point_id)
-		points[point_id].updateW();
+	for (size_t pointID = 0; pointID < points.size(); ++pointID)
+	{
+		points[pointID].updateW();
+	}
 }
 
-/*!
- * Checks if the algorithm converged.
- */
 bool FuzzyCMeans::isOver()
 {
 	bool over = true;
-	for (size_t point_id = 0; point_id < points.size(); ++point_id)
+	for (size_t pointID = 0; pointID < points.size(); ++pointID)
 	{
-		if (!points[point_id].compareW(eps))
+		if (!points[pointID].compareW(eps))
 		{
 			over = false;
 			break;
@@ -162,57 +177,62 @@ bool FuzzyCMeans::isOver()
 	return over;
 }
 
-/*!
- * Runs the Fuzzy c-means routine.
- */
 void FuzzyCMeans::runFuzzyCMeans()
 {
 	int start = clock();
 	size_t iter = 0;
-	double objective_value = 0.0;
-	for (iter = 0; iter < max_num_of_iterations; ++iter)
+	double objectiveValue = 0.0;
+	for (iter = 0; iter < maxNumOfIterations; ++iter)
 	{
-		objective_value = computeObjectiveFunction();
-		std::cout << "Iteration count = " << iter << ", obj. fcn = " << objective_value << std::endl;
+		objectiveValue = computeObjectiveFunction();
+		std::cout << "Iteration count = " << iter << ", obj. fcn = " << objectiveValue << std::endl;
 		computeClusters();
 		computeNewMembershipCoefficients();
 		if (isOver())
+		{
 			break;
+		}
 		else
+		{
 			updateMembershipCoefficients();
+		}
 	}
 	int stop = clock();
-	double elapsed_time = (stop - start) / static_cast<double>(CLOCKS_PER_SEC);
-	std::cout << "Fuzzy c-means finished after " << (iter + 1) << " iteration(s) (" << elapsed_time << " sec.).\n\n";
+	double elapsedTime = (stop - start) / static_cast<double>(CLOCKS_PER_SEC);
+	std::cout << "Fuzzy c-means finished after " << (iter + 1) << " iteration(s) (" << elapsedTime << " sec.).\n\n";
 }
 
 void FuzzyCMeans::printPartitionMatrix()
 {
 	std::cout << "Partition Matrix:\n\n";
-	for (size_t point_id = 0; point_id < points.size(); ++point_id)
-		points[point_id].printMembershipCoefficients();
+	for (size_t pointID = 0; pointID < points.size(); ++pointID)
+	{
+		points[pointID].printMembershipCoefficients();
+	}
 	std::cout << std::endl;
 }
 
 void FuzzyCMeans::printCentroids()
 {
 	std::cout << "Centroids:\n\n";
-	for (size_t cluster_id = 0; cluster_id < clusters.size(); ++cluster_id)
-		clusters[cluster_id].printCentroid();
+	for (size_t clusterID = 0; clusterID < clusters.size(); ++clusterID)
+	{
+		clusters[clusterID].printCentroid();
+	}
 	std::cout << std::endl;
 }
 
 double FuzzyCMeans::computeObjectiveFunction()
 {
-	double objective_value = 0.0;
-	for (size_t point_id = 0; point_id < points.size(); ++point_id)
+	double objectiveValue = 0.0;
+	for (size_t pointID = 0; pointID < points.size(); ++pointID)
 	{
-		for (size_t cluster_id = 0; cluster_id < clusters.size(); ++cluster_id)
+		for (size_t clusterID = 0; clusterID < clusters.size(); ++clusterID)
 		{
-			double dist = mfnc::computeEuclideanDistance(*(points[point_id].getValues()), *(clusters[cluster_id].getCentroid()));
+			double dist = mfnc::computeEuclideanDistance(*(points[pointID].getValues()), *(clusters[clusterID].getCentroid()));
 			dist = std::pow(dist, 2.0);
-			objective_value += (std::pow(points[point_id].getWOldValue(cluster_id), m) * dist);
+			objectiveValue += (std::pow(points[pointID].getWOldValue(clusterID), m) * dist);
 		}
 	}
-	return objective_value;
+	return objectiveValue;
 }
--- FuzzyCMeans.h
+++ FuzzyCMeans.h
@@ -7,38 +7,81 @@
 
 class FuzzyCMeans
 {
+	/*! the dimension of the input data (i.e., number of features) */
 	int	dimension;
+	/*! the number of clusters */
 	int k;
-	int max_num_of_iterations;
+	/*! the maximum number of iterations for the algorithm to converge*/
+	int maxNumOfIterations;
+	/*! fuzzifier */
 	int m;
+	/*! the threshold controlling the convergence of the algorithm */
 	double eps;
 public:
+	/*! the data to be clustered */
 	std::vector<Point> points;
+	/*! the clusters to be constructed */
 	std::vector<Cluster> clusters;
 public:
 
 	/*!
-	 * Constructor 1.
-	 */
-	FuzzyCMeans(std::string dataset_filename, std::string properties_fileName);
+ 	 * Constructor 1.
+ 	 * @param dataFilename the name of the file in which the data to be clustered are stored.
+ 	 * @param paramsFilename the name of the configuration file.
+ 	 */
+	FuzzyCMeans(std::string dataFilename, std::string paramsFilename);
 	
 	/*!
-	 * Constructor 2.
-	 */
-	FuzzyCMeans(std::string dataset_filename, int dimension_, int k_, int max_num_Of_iterations_, int m_, double eps_);
+ 	 * Constructor 2.
+ 	 * @param dataFilename the name of the file in which the data to be clustered are stored.
+ 	 * @param paramsFilename the name of the configuration file.
+ 	 */
+	FuzzyCMeans(std::string dataFilename, int dimension_, int k_, int maxNumOfIterations_, int m_, double eps_);
 	
 	/*!
 	 * Destructor.
 	 */
 	~FuzzyCMeans();
 	
+	/*!
+	 * It computes the clusters in every iteration of the algorithm.
+	 */
 	void computeClusters();
+
+	/*!
+	 * It computes the membership coefficients of points to clusters in every iteration of the algorithm.
+	 */
 	void computeNewMembershipCoefficients();
+
+	/*!
+	 * It updates the membership coefficients of points to clusters in every iteration of the algorithm.
+	 */
 	void updateMembershipCoefficients();
+	
+	/*!
+	 * It checks if the algorithm converged.
+	 * @return true if the algorithm converged, false otherwise.
+	 */
 	bool isOver();
+
+	/*!
+ 	 * It runs the Fuzzy c-means routine.
+ 	 */
 	void runFuzzyCMeans();
+
+	/*!
+ 	 * It prints the partition matrix to the standard output.
+ 	 */
 	void printPartitionMatrix();
+
+	/*!
+ 	 * It prints the centroids to the standard output.
+ 	 */
 	void printCentroids();
+
+	/*!
+ 	 * It computes the value of the objective function that controls the convergence of the algorithm.
+ 	 */
 	double computeObjectiveFunction();
 };
 
