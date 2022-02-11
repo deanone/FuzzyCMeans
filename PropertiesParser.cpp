d9b30c9331f3a915348d9ce89549b58fb14833be
deanone <athanasios.salamanis@gmail.com>
2022-02-11 15:42:51 +0200

    Minor refactoring (6).

--- PropertiesParser.cpp
+++ PropertiesParser.cpp
@@ -1,201 +1,135 @@
-/*!
- * PropertiesParser.cpp: implementation of the PropertiesParser class.
- */
-
-#include "PropertiesParser.h"
 #include <fstream>
 #include <sstream>
 
-/*!
- * Constructor. Initializes the parser parsing the properties file and
- * reading all properties in the \c properties_map.
- *
- * @param propertiesFilename the filename of the properties file.
- */
-PropertiesParser::PropertiesParser(std::string properties_filename)
+#include "PropertiesParser.h"
+
+PropertiesParser::PropertiesParser(std::string propertiesFilename)
 {
-	addPropertiesFile(properties_filename);
+	addPropertiesFile(propertiesFilename);
 }
 
-/*!
- * Destructor. Destroys the map object properties_map of the class and frees the corresponding memory.
- */
-PropertiesParser::~PropertiesParser()
+void PropertiesParser::addPropertiesFile(std::string propertiesFilename)
 {
-	properties_map.clear();
-}
-
-/*!
- * Parses a properties file and reads all properties in the \c properties_map.
- * It may be used consecutively to parse multiple properties files.
- *
- * @param propertiesFilename the filename of the properties file.
- */
-void PropertiesParser::addPropertiesFile(std::string properties_filename)
-{
-	std::ifstream properties_file_stream(properties_filename);
+	std::ifstream properties_file;
+	properties_file.open(propertiesFilename.c_str(), std::ios::in);
 	std::string line;
-	if (properties_file_stream.is_open())
+	if (properties_file.good())
 	{
-		if (properties_file_stream.good())
+		while (properties_file.eof() == false)
 		{
-			while (properties_file_stream.eof() == false)
+			line.clear();
+			getline(properties_file, line);
+			if (line.length() > 0 && line[0] != '#')
 			{
-				line.clear();
-				std::getline(properties_file_stream, line);
-				if (line.length() > 0 && line[0] != '#')	/*! line is not a comment line*/
-				{
-					std::string property_name;
-					std::string property_value;
-					std::stringstream ss(line);
-					char c;//c used to eat the '=' character
-					ss >> property_name >> c >> property_value;
-					properties_map[property_name] = property_value;
-				}
+				std::string propertyName;
+				std::string propertyValue;
+				std::stringstream ss(line);
+				char c;//c used to eat the '=' character
+				ss >> propertyName >> c >> propertyValue;
+				propMap[propertyName] = propertyValue;
 			}
 		}
-		properties_file_stream.close();
+	}
+	properties_file.close();
+}
+
+bool PropertiesParser::propertyExists(std::string propertyName)
+{
+	return (!(propMap.find(propertyName) == propMap.end()));
+}
+
+std::string PropertiesParser::getPropertyAsString(std::string propertyName)
+{
+	return propMap[propertyName];
+}
+
+int PropertiesParser::getPropertyAsInt(std::string propertyName)
+{
+	int propertyValue;
+	std::stringstream strStream(propMap[propertyName]);
+	strStream >> propertyValue;
+	return propertyValue;
+}
+
+bool PropertiesParser::getPropertyAsBool(std::string propertyName)
+{
+	bool propertyValue;
+	propertyValue = (propMap[propertyName] == "true");
+	return propertyValue;
+}
+
+float PropertiesParser::getPropertyAsFloat(std::string propertyName)
+{
+	float propertyValue;
+	std::stringstream strStream(propMap[propertyName]);
+	strStream >> propertyValue;
+	return propertyValue;
+}
+
+double PropertiesParser::getPropertyAsDouble(std::string propertyName)
+{
+	double propertyValue;
+	std::stringstream strStream(propMap[propertyName]);
+	strStream >> propertyValue;
+	return propertyValue;
+}
+
+std::string PropertiesParser::getPropertyAsStringOrDefaultTo(std::string propertyName, std::string defaultPropertyValue)
+{
+	if (propertyExists(propertyName))
+	{
+		return getPropertyAsString(propertyName);
+	}
+	else
+	{
+		return defaultPropertyValue;
 	}
 }
 
-/*!
- * Checks if a property exists.
- *
- * @param property_name the name of the property to be checked.
- * @return \c true if property exists, \c false otherwise.
- */
-bool PropertiesParser::propertyExists(std::string property_name)
+int PropertiesParser::getPropertyAsIntOrDefaultTo(std::string propertyName, int defaultPropertyValue)
 {
-	return (!(properties_map.find(property_name) == properties_map.end()));
+	if (propertyExists(propertyName))
+	{
+		return getPropertyAsInt(propertyName);
+	}
+	else
+	{
+		return defaultPropertyValue;
+	}
 }
 
-/*!
- * Returns the string value of a property.
- *
- * @param property_name the name of the property of which the value is returned.
- * @return a string value for the property.
- */
-std::string PropertiesParser::getPropertyAsString(std::string property_name)
+bool PropertiesParser::getPropertyAsBoolOrDefaultTo(std::string propertyName, bool defaultPropertyValue)
 {
-	return properties_map[property_name];
+	if (propertyExists(propertyName))
+	{
+		return getPropertyAsBool(propertyName);
+	}
+	else
+	{
+		return defaultPropertyValue;
+	}
 }
 
-/*!
- * Returns the integer value of a property.
- *
- * @param property_name the name of the property of which the value is returned.
- * @return an integer value for the property.
- */
-int PropertiesParser::getPropertyAsInt(std::string property_name)
+float PropertiesParser::getPropertyAsFloatOrDefaultTo(std::string propertyName, float defaultPropertyValue)
 {
-	int property_value = 0;
-	std::stringstream str_stream(properties_map[property_name]);
-	str_stream >> property_value;
-	return property_value;
+	if (propertyExists(propertyName))
+	{
+		return getPropertyAsFloat(propertyName);
+	}
+	else
+	{
+		return defaultPropertyValue;
+	}
 }
 
-/*!
- * Returns the boolean value of a property.
- *
- * @param property_name the name of the property of which the value is returned.
- * @return a boolean value for the property.
- */
-bool PropertiesParser::getPropertyAsBool(std::string property_name)
+double PropertiesParser::getPropertyAsDoubleOrDefaultTo(std::string propertyName, double defaultPropertyValue)
 {
-	bool property_value = true;
-	property_value = (properties_map[property_name] == "true");
-	return property_value;
-}
-
-/*!
- * Returns the float value of a property.
- *
- * @param property_name the name of the property of which the value is returned.
- * @return a float value for the property.
- */
-float PropertiesParser::getPropertyAsFloat(std::string property_name)
-{
-	float property_value = 0.0f;
-	std::stringstream str_stream(properties_map[property_name]);
-	str_stream >> property_value;
-	return property_value;
-}
-
-/*!
- * Returns the double value of a property.
- *
- * @param property_name the name of the property of which the value is returned.
- * @return a double value for the property.
- */
-double PropertiesParser::getPropertyAsDouble(std::string property_name)
-{
-	double property_value = 0.0;
-	std::stringstream str_stream(properties_map[property_name]);
-	str_stream >> property_value;
-	return property_value;
-}
-
-/*!
- * Returns the string value of a property, if it exists, or a default value if
- * the property does not exist.
- *
- * @param property_name the name of the property of which the value is returned.
- * @param defaultproperty_value the default property value.
- * @return a string value for the property.
- */
-std::string PropertiesParser::getPropertyAsStringOrDefaultTo(std::string property_name, std::string default_property_value)
-{
-	return propertyExists(property_name) ? getPropertyAsString(property_name) : default_property_value;
-}
-
-/*!
- * Returns the integer value of a property, if it exists, or a default value if
- * the property does not exist.
- *
- * @param property_name the name of the property of which the value is returned.
- * @param defaultproperty_value the default property value.
- * @return an integer value for the property.
- */
-int PropertiesParser::getPropertyAsIntOrDefaultTo(std::string property_name, int default_property_value)
-{
-	return propertyExists(property_name) ? getPropertyAsInt(property_name) : default_property_value;
-}
-
-/*!
- * Returns the boolean value of a property, if it exists, or a default value if
- * the property does not exist.
- *
- * @param property_name the name of the property of which the value is returned.
- * @param defaultproperty_value the default property value.
- * @return a boolean value for the property.
- */
-bool PropertiesParser::getPropertyAsBoolOrDefaultTo(std::string property_name, bool default_property_value)
-{
-	return propertyExists(property_name) ? getPropertyAsBool(property_name) : default_property_value;
-}
-
-/*!
- * Returns the float value of a property, if it exists, or a default value if
- * the property does not exist.
- *
- * @param property_name the name of the property of which the value is returned.
- * @param defaultproperty_value the default property value.
- * @return a float value for the property.
- */
-float PropertiesParser::getPropertyAsFloatOrDefaultTo(std::string property_name, float default_property_value)
-{
-	return propertyExists(property_name) ? getPropertyAsFloat(property_name) : default_property_value;
-}
-
-/*!
- * Returns the double value of a property, if it exists, or a default value if
- * the property does not exist.
- * 
- * @param property_name the name of the property of which the value is returned.
- * @param defaultproperty_value the default property value.
- * @return a double value for the property.
- */
-double PropertiesParser::getPropertyAsDoubleOrDefaultTo(std::string property_name, double default_property_value)
-{
-	return propertyExists(property_name) ? getPropertyAsDouble(property_name) : default_property_value;
+	if (propertyExists(propertyName))
+	{
+		return getPropertyAsDouble(propertyName);
+	}
+	else
+	{
+		return defaultPropertyValue;
+	}
 }
--- PropertiesParser.h
+++ PropertiesParser.h
@@ -1,33 +1,110 @@
-/*!
- * PropertiesParser.h : interface of the PropertiesParser class.
- * Using this class one can read a properties file which contains values in the following form:
- * [propertyName] = [propertyValue]
- */
-
 #ifndef PROPERTIES_PARSER_H
 #define PROPERTIES_PARSER_H
 
-#include <string>
 #include <map>
 
 class PropertiesParser
 {
-	std::map<std::string, std::string> properties_map;
+protected:
+	
+	/*!
+	 * A map containing property names as keys and property values as values.
+	 */
+	std::map<std::string, std::string> propMap;
+
 public:
-	PropertiesParser(std::string properties_filename);
-	~PropertiesParser();
-	void addPropertiesFile(std::string properties_filename);
-	bool propertyExists(std::string property_name);
-	std::string getPropertyAsString(std::string property_name);
-	int getPropertyAsInt(std::string property_name);
-	bool getPropertyAsBool(std::string property_name);
-	float getPropertyAsFloat(std::string property_name);
-	double getPropertyAsDouble(std::string property_name);
-	std::string getPropertyAsStringOrDefaultTo(std::string property_name, std::string default_property_value);
-	int getPropertyAsIntOrDefaultTo(std::string property_name, int default_property_value);
-	bool getPropertyAsBoolOrDefaultTo(std::string property_name, bool default_property_value);
-	float getPropertyAsFloatOrDefaultTo(std::string property_name, float default_property_value);
-	double getPropertyAsDoubleOrDefaultTo(std::string property_name, double default_property_value);
+	/*!
+	 * Constructor.
+	 */
+	PropertiesParser(std::string propertiesFilename);
+	
+	/*!
+	 * Creates the properties map from the properties contained in a file.
+	 * @param propertiesFilename the name of the file that contains the properties.
+	 */
+	void addPropertiesFile(std::string propertiesFilename);
+	
+	/*!
+	 * Checks if a specific property exists in the properties map.
+	 * @param propertyName the name of the property to be checked.
+	 * @return true if property exists in the properties map, false otherwise.
+	 */
+	bool propertyExists(std::string propertyName);
+	
+	/*!
+	 * Returns a property as type string.
+	 * @param propertyName the name of the property.
+	 * @return the string type property.
+	 */
+	std::string getPropertyAsString(std::string propertyName);
+	
+	/*!
+	 * Returns a property as type int.
+	 * @param propertyName the name of the property.
+	 * @return the int type property.
+	 */
+	int getPropertyAsInt(std::string propertyName);
+	
+	/*!
+	 * Returns a property as type bool.
+	 * @param propertyName the name of the property.
+	 * @return the bool type property.
+	 */
+	bool getPropertyAsBool(std::string propertyName);
+	
+	/*!
+	 * Returns a property as type float.
+	 * @param propertyName the name of the property.
+	 * @return the float type property.
+	 */
+	float getPropertyAsFloat(std::string propertyName);
+	
+	/*!
+	 * Returns a property as type double.
+	 * @param propertyName the name of the property.
+	 * @return the double type property.
+	 */
+	double getPropertyAsDouble(std::string propertyName);
+	
+	/*!
+	 * Returns a property as type string, or return a default string type value.
+	 * @param propertyName the name of the property.
+	 * @param defaultPropertyValue the default value of the property
+	 * @return the string type property, or a default string type value.
+	 */
+	std::string getPropertyAsStringOrDefaultTo(std::string propertyName, std::string defaultPropertyValue);
+	
+	/*!
+	 * Returns a property as type int, or return a default int type value.
+	 * @param propertyName the name of the property.
+	 * @param defaultPropertyValue the default value of the property
+	 * @return the int type property, or a default int type value.
+	 */
+	int getPropertyAsIntOrDefaultTo(std::string propertyName, int defaultPropertyValue);
+	
+	/*!
+	 * Returns a property as type bool, or return a default bool type value.
+	 * @param propertyName the name of the property.
+	 * @param defaultPropertyValue the default value of the property
+	 * @return the bool type property, or a default bool type value.
+	 */
+	bool getPropertyAsBoolOrDefaultTo(std::string propertyName, bool defaultPropertyValue);
+	
+	/*!
+	 * Returns a property as type float, or return a default float type value.
+	 * @param propertyName the name of the property.
+	 * @param defaultPropertyValue the default value of the property
+	 * @return the int float property, or a default float type value.
+	 */
+	float getPropertyAsFloatOrDefaultTo(std::string propertyName, float defaultPropertyValue);
+	
+	/*!
+	 * Returns a property as type double, or return a default double type value.
+	 * @param propertyName the name of the property.
+	 * @param defaultPropertyValue the default value of the property
+	 * @return the double type property, or a default double type value.
+	 */
+	double getPropertyAsDoubleOrDefaultTo(std::string propertyName, double defaultPropertyValue);
 };
 
-#endif	//	!PROPERTIES_PARSER_H
+#endif	//	PROPERTIES_PARSER_H
